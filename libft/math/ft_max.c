c501bd2101ec47b53d45ff7b0605e88d41c46390
youpaw <drozdov.vov@gmail.com>
2020-03-11 11:00:16 +0300

    libft removed

--- libft/math/ft_max.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-ssize_t ft_max(ssize_t x, ssize_t y)
-{
-	if (x < y)
-		return (y);
-	return (x);
-}
--- libft/math/ft_min.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-ssize_t ft_min(ssize_t x, ssize_t y)
-{
-	if (x > y)
-		return (y);
-	return (x);
-}
