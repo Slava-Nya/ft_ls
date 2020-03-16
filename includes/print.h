e525c45ad39ebc96805abd1a4b7b7d6b2826ff62
youpaw <drozdov.vov@gmail.com>
2020-03-16 21:13:17 +0300

    print module init

---
+++ includes/print.h
@@ -0,0 +1,12 @@
+//
+// Created by youpaw on 16.03.2020.
+//
+
+#ifndef PRINT_H
+#define PRINT_H
+
+#include "nodes.h"
+
+void	print_node(t_node *node, t_flags *flags);
+
+#endif //PRINT_H
---
+++ src/print_args.c
@@ -0,0 +1,27 @@
+//
+// Created by youpaw on 16.03.2020.
+//
+
+#include "flags.h"
+#include "list_lib.h"
+#include "print.h"
+
+static void	del(t_node *node, size_t size)
+{
+	(void) size;
+
+}
+
+void		print_args(t_list **nodes, t_flags *flags)
+{
+	t_list *head;
+
+	head = *nodes;
+	while (head)
+	{
+		print_node(head->content, flags);
+		head = head->next;
+		ft_lstdel(nodes, );
+		*nodes = head;
+	}
+}
