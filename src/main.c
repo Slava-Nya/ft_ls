c93df6e1fc3e0245e654105b753a8ea66e571cdc
dbutterw <dbutterw@student.21-school.ru>
2020-07-17 23:23:10 +0300

    quick sort fix

--- src/main.c
+++ src/main.c
@@ -6,12 +6,14 @@
 /*   By: slavanya <slavanya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 00:00:11 by slavanya          #+#    #+#             */
-/*   Updated: 2020/07/17 21:04:52 by dbutterw         ###   ########.fr       */
+/*   Updated: 2020/07/17 23:05:48 by dbutterw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nodes.h"
 #include "ls.h"
+#include "sort_lib.h"
+#include "str_lib.h"
 
 int     main(int argc, char **argv)
 {
@@ -20,13 +22,15 @@ int     main(int argc, char **argv)
 	t_list	*nodes;
 
 	flags = init_flags();
-    skip = parse_flags(flags, argc, argv);
-    nodes = parse_args(argc, argv, skip, flags);
-    if (nodes)
+	skip = parse_flags(flags, argc, argv);
+	quick_sort((void **) argv, skip, argc - 1, \
+		(int (*)(const void *, const void *)) ft_strcmp);
+	nodes = parse_args(argc, argv, skip, flags);
+	if (nodes)
 	{
 		print_args(nodes, flags);
 		ft_lstdelone(&nodes, (void (*)(void *)) &del_node);
 	}
-    del_flags(&flags);
-    return (0);
+	del_flags(&flags);
+	return (0);
 }
